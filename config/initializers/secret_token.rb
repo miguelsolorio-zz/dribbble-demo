e519d078feeefd28b297f2d4a6bc9935632a51ac
Miguel Solorio <miguel.solorio07@gmail.com>
2013-07-06 21:13:51 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dribbble::Application.config.secret_token = '3aee1ff883335cf2496af6c2f45e6d9c3b762aa5317226214736bc87f699c6cc39b81c4e24daa477e4317e7c7fbede251c9ab4604f9269fe422fde775e50a252'
