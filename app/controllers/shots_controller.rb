8764c932c83a3f54b4cf731d59aa22fd7699a6a4
Miguel Solorio <miguel.solorio07@gmail.com>
2013-07-07 19:07:46 -0700

    Add Pagination

--- app/controllers/shots_controller.rb
+++ app/controllers/shots_controller.rb
@@ -4,7 +4,7 @@ class ShotsController < ApplicationController
   # GET /shots
   # GET /shots.json
   def index
-    @shots = Shot.order('created_at desc')
+    @shots = Shot.order('created_at desc').page(params[:page]).per_page(20)
 
     respond_to do |format|
       format.html # index.html.erb
